3c4b35fb1739f260194bf088e27c8db687ec8b83
J Shwiizzle <160753124+SwagAccount@users.noreply.github.com>
2025-04-11 15:50:24 +0100

    Create MapLoader.cs

---
+++ Code/GameManagement/MapLoader.cs
@@ -0,0 +1,123 @@
+using Sandbox;
+using static Sandbox.Gizmo;
+
+namespace Seekers;
+
+public sealed class MapLoader : MapInstance
+{
+	[Sync] public bool IsLoadingMap { get; set; }
+	public bool ClientIsLoadingMap { get; set; }
+
+	List<(GameObject prop, Transform pos)> _propStartTransforms = [];
+
+	public static MapLoader Instance;
+
+	protected override void OnStart()
+	{
+		Instance = this;
+		OnMapLoaded += OnMapLoad;
+		TagProps();
+	}
+
+	public void SoftReset()
+	{
+
+	}
+
+	public void OnMapLoad()
+	{
+		ClientIsLoadingMap = false;
+
+		TagProps();
+
+		if ( Networking.IsHost )
+		{
+			IsLoadingMap = false;
+		}
+	}
+
+	[Rpc.Broadcast]
+	void TagProps()
+	{
+		_propStartTransforms.Clear();
+
+		var allProps = Components.GetAll<Prop>( FindMode.EverythingInSelfAndDescendants );
+
+		foreach ( var prop in allProps )
+		{
+			if ( !prop.IsValid() )
+				continue;
+
+			if ( !prop.Tags.Contains( "worldprop" ) )
+				prop.Tags.Add( "worldprop" );
+
+			_propStartTransforms.Add( (prop.GameObject, prop.WorldTransform) );
+		}
+	}
+
+	[Rpc.Broadcast]
+	public async static void ChangeMap( string map )
+	{
+		Instance.ClientIsLoadingMap = true;
+
+		if ( Instance.MapName != map )
+		{
+			await LoadMap( map );
+		}
+		else
+		{
+			Instance.IsLoadingMap = true;
+			Instance.ClientIsLoadingMap = true;
+			Instance.OnMapLoaded();
+		}
+
+		foreach ( var envmap in Instance.GetComponentsInChildren<EnvmapProbe>() )
+		{
+			envmap.Dirty = true;
+		}
+	}
+
+	async static Task LoadMap( string map )
+	{
+		foreach ( var gameObject in Instance.GameObject.Children )
+		{
+			gameObject.Destroy();
+		}
+
+		var sceneLoadOptions = new SceneLoadOptions { IsAdditive = true };
+
+		var mapPackage = await Package.FetchAsync( map, false );
+
+		if ( mapPackage == null ) return;
+
+		var primaryAsset = mapPackage.GetMeta<string>( "PrimaryAsset" );
+
+		if ( primaryAsset.EndsWith( ".scene" ) )
+		{
+			await mapPackage.MountAsync();
+			Instance.UnloadMap();
+			Instance.Enabled = false;
+			var sceneFile = mapPackage.GetMeta<SceneFile>( "PrimaryAsset" );
+			sceneLoadOptions.SetScene( sceneFile );
+			List<GameObject> NonMapObjects = new();
+			foreach(var gameobject in Game.ActiveScene.Children)
+			{
+				NonMapObjects.Add( gameobject );
+			}
+			Game.ActiveScene.Load( sceneLoadOptions );
+			var children = new List<GameObject>(Game.ActiveScene.Children);
+			foreach ( var gameobject in children )
+			{
+				if ( NonMapObjects.Contains( gameobject ) )
+					continue;
+				gameobject.SetParent( Instance.GameObject );
+			}
+			Instance.OnMapLoaded();
+		}
+		else
+		{
+			Instance.Enabled = true;
+			Instance.MapName = map;
+		}
+	}
+}
