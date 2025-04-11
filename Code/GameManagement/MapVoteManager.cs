b90e62898acc41ad65bf2f339705c5d7a540b722
J Shwiizzle <160753124+SwagAccount@users.noreply.github.com>
2025-04-11 16:19:13 +0100

    Map Voting

---
+++ Code/GameManagement/MapVoteManager.cs
@@ -0,0 +1,40 @@
+using Sandbox;
+
+namespace Seekers;
+
+public sealed class MapVoteManager : SingletonComponent<MapVoteManager>
+{
+	public bool Voting;
+	public void StartVote()
+	{
+		Voting = true;
+	}
+	[Property] public Dictionary<SteamId, string> Votes { get; set; } = new();
+	public string GetMap()
+	{
+		var votes = Votes.Values.GroupBy( x => x )
+					.Select( g => new { Map = g.Key, Count = g.Count() } )
+					.OrderByDescending( x => x.Count )
+					.FirstOrDefault();
+
+		Voting = false;
+
+		return votes?.Map ?? null;
+	}
+
+	[Rpc.Broadcast]
+	public void AddVote( Client client, string map )
+	{
+		Log.Info( "Vote added for " + map );
+
+		// Remove the old vote if it exists
+		if ( client.CurrentVote != null )
+		{
+			Votes.Remove( client.SteamId );
+		}
+
+		// Add the new vote
+		Votes[client.SteamId] = map;
+		client.CurrentVote = map;
+	}
+}
