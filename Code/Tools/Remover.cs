0a550127fe49302774b07c35f62266d3cd0b118a
trende2001 <68961063+trende2001@users.noreply.github.com>
2025-08-28 15:31:44 +0200

    Current Tool Display UI

--- Code/Tools/Remover.cs
+++ Code/Tools/Remover.cs
@@ -1,16 +1,19 @@
 namespace Seekers;
 
-[Library( "tool_remover", Description = "Remove entities")]
+[Library( "tool_remover", Description = "Remove entities" )]
 [Group( "construction" )]
 public partial class Remover : BaseTool
 {
 	public override bool UseGrid => false;
+
 	public override bool Primary( SceneTraceResult trace )
 	{
 		if ( Input.Pressed( "attack1" ) )
 		{
+			if ( !trace.GameObject.IsValid() )
+				return false;
 
-			if (trace.GameObject.Components.TryGet<PropHelper>(out PropHelper helper))
+			if ( trace.GameObject.Components.TryGet( out PropHelper helper ) )
 				trace.GameObject.BroadcastDestroy();
 
 			return true;
