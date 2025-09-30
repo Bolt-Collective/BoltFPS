9f3a1775265c8d327bd1cd8d5bea1a5da5fcef46
trende2001 <68961063+trende2001@users.noreply.github.com>
2025-09-30 15:52:47 +0200

    Added base commands
    - Toast command
    - Kill command
    - Respawn Entities command (`respawn_entities`)

---
+++ Code/GameManagement/BaseGameManager.Commands.cs
@@ -0,0 +1,32 @@
+﻿using BoltFPS;
+
+namespace Seekers;
+
+public partial class BaseGameManager
+{
+	[ConCmd( "toast" )]
+	public static void DisplayToast( string text, float duration = 5 )
+	{
+		ToastNotification.Current?.AddToast( text, duration );
+	}
+	
+	[ConCmd( "kill" )]
+	public static void Suicide()
+	{
+		Pawn.Local.TakeDamage( 1000 );
+	}
+	
+	[ConCmd( "respawn_entities" )]
+	public static void Cleanup()
+	{
+		foreach ( var component in Game.ActiveScene.GetAll<DestroyOnMapCleanup>() )
+		{
+			component.GameObject.Destroy();
+		}
+
+		if ( MapLoader.Instance != null )
+		{
+			MapLoader.Instance.Cleanup();
+		}
+	}
+}
